b2b678e16b7df1a9cf454c5cbe66b0b9694ef53c
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-29 18:05:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,10 @@
 def starts_with_a_vowel?(word)
 word.each do |vow|
 return true if   vow.match(/^[aeiouAEIOU]\w+/)
+else false 
 end
+end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 
